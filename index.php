650558c227dacf644a73d53b1af51b782427c098
Adrián Esneyder Salcedo Otero <adrianeso@gmail.com>
2024-01-22 10:41:47 +0100

    initial changes project

---
+++ index.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;700;800&display=swap" rel="stylesheet">
+    <title>Spa Center</title>
+</head>
+<body>
+
+<!--    heading of page-->
+    <header id="main-header">
+
+        <div class="topbar">
+            <div class="contact">
+                <span><ion-icon name="call-sharp"></ion-icon>phone</span>
+                <span> <ion-icon name="mail-sharp"></ion-icon>email</span>
+            </div>
+            <div class="socialnet">
+                <span>f</span>
+                <span>y</span>
+                <span>l</span>
+                <span>i</span>
+                <span>x</span>
+            </div>
+        </div>
+
+        <nav class="navbar">
+            <div class="logo">
+                <span class="logo-text logo-spa">spa</span>
+                <span class="logo-text logo-center">center</span>
+            </div>
+
+            <div class="main-menu-bar">
+                <ul class="main-menu-list">
+                    <li class="main-menu-item">
+                        <a class="main-menu-link" href="">home</a>
+                    </li>
+                    <li class="main-menu-item">
+                        <a class="main-menu-link" href="">about</a>
+                    </li>
+                    <li class="main-menu-item">
+                        <a class="main-menu-link" href="">services</a>
+                    </li>
+                    <li class="main-menu-item">
+                        <a class="main-menu-link" href="">pricing</a>
+                    </li>
+                    <li class="main-menu-item">
+                        <a class="main-menu-link" href="">contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="button-appointment">
+                <a href="">Make appointment</a>
+            </div>
+        </nav>
+
+    </header>
+
+<!--heroimage-->
+    <div id="hero-section">
+        <div>
+            <p>text</p>
+            <h2>Treatment</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.ium officia</p>
+            <a href="#">make appointment</a>
+        </div>
+    </div>
+
+<!--service description-->
+    <div>
+        <div>image</div>
+        <article>
+            <div>
+                about text
+            </div>
+            <div>
+                title service
+            </div>
+            <div>
+                <p>descripction</p>
+            </div>
+            <div>
+                <div>
+                    100
+                    specialists
+                </div>
+                <div>
+                    100
+                    happy clients
+                </div>
+            </div>
+        </article>
+    </div>
+
+<!--services section-->
+    <div>
+        <div>title & desc</div>
+        <div>
+            <div>services</div>
+            <div>section form</div>
+        </div>
+    </div>
+
+
+
+
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+</body>
+</html>
